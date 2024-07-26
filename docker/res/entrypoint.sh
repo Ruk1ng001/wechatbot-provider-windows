d2aa152ae944a7ca70ad4e51058cde41718d98ce
Ruk1ng <57079399+Ruk1ng001@users.noreply.github.com>
2024-07-26 23:05:59 +0800

    Update entrypoint.sh

--- docker/res/entrypoint.sh
+++ docker/res/entrypoint.sh
@@ -1,9 +1,20 @@
 #!/bin/sh
 
-rm /var/run/xrdp.pid
+# 删除 /var/run/xrdp.pid 文件（如果存在）
+if [ -f /var/run/xrdp.pid ]; then
+    rm /var/run/xrdp.pid
+fi
+
+# 启动 xrdp 服务
 /usr/sbin/xrdp
-rm /var/run/xrdp-sesman.pid
+
+# 删除 /var/run/xrdp-sesman.pid 文件（如果存在）
+if [ -f /var/run/xrdp-sesman.pid ]; then
+    rm /var/run/xrdp-sesman.pid
+fi
+
+# 启动 xrdp-sesman 服务
 /usr/sbin/xrdp-sesman
 
-# Necesssary for docker
+# 使 Docker 能够接收传递的命令
 exec "$@"
